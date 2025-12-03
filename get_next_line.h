9fa256be6cb54385a85961a111525a254d024968
Marc-Antoine Berger <marberge@z1r9p4.42lyon.fr>
2025-12-03 15:32:30 +0100

    dev

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 10:32:46 by marberge          #+#    #+#             */
-/*   Updated: 2025/12/03 10:56:30 by marberge         ###   ########.fr       */
+/*   Updated: 2025/12/03 15:16:40 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 6
+#  define BUFFER_SIZE 5
 # endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char const *s2);
 char	*ft_trim_from_start(char *str);
 char	*ft_strdup(const char *s);
+int		ft_search_char(char *str);
+char	*ft_resize_stash(char *str);
+size_t	ft_strlen(const char *s);
 
 #endif
