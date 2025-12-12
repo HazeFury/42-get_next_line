b13492d056575296a013bbcf62c351eec028ccb2
Marc-Antoine Berger <marberge@z3r9p1.42lyon.fr>
2025-12-12 12:45:30 +0100

    finally works fine. Refactor and norminette remains

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 10:34:46 by marberge          #+#    #+#             */
-/*   Updated: 2025/12/11 16:56:22 by marberge         ###   ########.fr       */
+/*   Updated: 2025/12/12 12:43:38 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,108 +28,63 @@ int ft_search_char(char *str)
 	return (-1);
 }
 
-void ft_resize_stash(char *str, char *tmp)
+void ft_resize_stash(char *tmp, char *stash)
 {
 	int i;
-	char *new_stash;
 	int k;
 
 	k = 0;
-	i = ft_search_char(str);
+	i = ft_search_char(tmp);
 	if (i != -1)
 	{
-		while (k < BUFFER_SIZE + 1)
+		i++;
+		while (k < BUFFER_SIZE)
 		{
-			if (str[i + 1] != '\0')
-				tmp[k] = str[i + 1];
+			if (tmp[i] != '\0')
+			{
+				stash[k] = tmp[i];
+				i++;
+			}
 			else
-				tmp[k] = '\0';
+				stash[k] = '\0';
 			k++;
 		}
 	}
 	else
-		tmp[0] = '\0';
-	free(str);
+		ft_bzero(stash, BUFFER_SIZE + 1);
+	free(tmp);
 	return;
 }
 
-// char	*get_next_line(int fd)
-// {
-// 	static char	*stash;
-// 	char		*buffer;
-// 	int			nb_read;
-// 	char		*line;
-
-// 	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
-// 		return (NULL);
-// 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-// 	if (!buffer)
-// 	{
-// 		if (stash)
-//     	{
-//         	free(stash);
-//         	stash = NULL;
-// 		}
-// 		return (NULL);
-// 	}
-// 	nb_read = 1;
-// 	if ((ft_search_char(stash) == -1) && nb_read > 0)
-// 	{
-// 		while (nb_read > 0)
-// 		{
-// 			nb_read = read(fd, buffer, BUFFER_SIZE);
-
-// 			if (nb_read == -1)
-// 			{
-// 				free(stash);
-// 				stash = NULL;
-// 				free(buffer);
-// 				return (NULL);
-// 			}
-// 			buffer[nb_read] = '\0';
-// 			stash = ft_strjoin(stash, buffer);
-// 			if (!stash)
-// 			{
-// 				free(buffer);
-// 				return (NULL);
-// 			}
-// 			if (ft_search_char(buffer) != -1)
-// 				break ;
-// 		}
-// 	}
-// 	line = ft_trim_from_start(stash);
-// 	if (!line)
-// 	{
-// 		free(stash);
-// 		stash = NULL;
-// 	}
-// 	else
-// 		stash = ft_resize_stash(stash);
-// 	free (buffer);
-// 	return (line);
-// }
-
 char *get_next_line(int fd)
 {
-	char *tmp;
-	static char stash[BUFFER_SIZE + 1];
-	char *buffer[BUFFER_SIZE + 1];
-	int nb_read;
-	char *line;
+	char 		*tmp;
+	static char	stash[BUFFER_SIZE + 1];
+	char 		*buffer;
+	int 		nb_read;
+	char 		*line;
 
+	tmp = NULL;
 	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
 		return (NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
 	nb_read = 1;
+	if (ft_strlen(stash) > 0)
+		tmp = ft_strjoin(tmp, stash);
 	while (ft_search_char(tmp) == -1 && nb_read > 0)
 	{
 		nb_read = read(fd, buffer, BUFFER_SIZE);
 		if (nb_read == -1)
 		{
+			free(buffer);
 			if (tmp)
 			{
 				free(tmp);
 				tmp = NULL;
 			}
+			ft_bzero(stash, BUFFER_SIZE + 1);
 			return (NULL);
 		}
 		if (nb_read > 0)
@@ -137,7 +92,10 @@ char *get_next_line(int fd)
 			buffer[nb_read] = '\0';
 			tmp = ft_strjoin(tmp, buffer);
 			if (!tmp)
+			{
+				free(buffer);
 				return (NULL);
+			}
 			if (ft_search_char(buffer) != -1)
 				break;
 		}
@@ -154,26 +112,26 @@ char *get_next_line(int fd)
 	return (line);
 }
 
-int main(void)
-{
-	int fd;
-	char *test;
-	int i;
+// int main(void)
+// {
+// 	int fd;
+// 	char *test;
+// 	int i;
 
-	i = 0;
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	// while (i < 8)
-	// {
-	// 	test = get_next_line(fd);
-	// 	printf("%s", test);
-	// 	free(test);
-	// 	i++;
-	// }
-	test = get_next_line(fd);
-	printf("%s", test);
-	free(test);
-	close(fd);
-	return (0);
-}
+// 	i = 0;
+// 	fd = open("test.txt", O_RDONLY);
+// 	if (fd < 0)
+// 		return (-1);
+// 	while (i < 8)
+// 	{
+// 		test = get_next_line(fd);
+// 		printf("%s", test);
+// 		free(test);
+// 		i++;
+// 	}
+// 	// test = get_next_line(fd);
+// 	// printf("%s", test);
+// 	// free(test);
+// 	close(fd);
+// 	return (0);
+// }
